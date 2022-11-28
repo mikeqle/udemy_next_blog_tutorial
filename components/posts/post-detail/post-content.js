3b22fb0afc92efbe0cf90a115f1d14521726dc27
Mike Le <mike.le@live.com>
2022-11-28 14:13:30 -0800

    added commented out tutorial code - does not seem to work with react-markdown 8

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -20,6 +20,29 @@ function PostContent(props) {
         />
       );
     },
+
+    
+    // p(paragraph) {
+    //   const { node } = paragraph;
+
+    //   if (node.children[0].type === "image") {
+    //     const image = node.children[0];
+
+    //     return (
+    //       <div className={classes.image}>
+    //         <Image
+    //           src={`/images/posts/${post.slug}/${image.url}`}
+    //           alt={image.alt}
+    //           width={600}
+    //           height={300}
+    //         />
+    //       </div>
+    //     );
+    //   }
+
+    //   return <p>{paragraph.children}</p>
+    // },
+
   };
 
   return (
