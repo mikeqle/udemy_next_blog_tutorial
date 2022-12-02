1fc9d00fcb7dcdb718e7a1cb406704671c345c7a
Mike Le <mike.le@live.com>
2022-12-01 16:46:50 -0800

    added Head tags to all pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,21 @@
+import Head from "next/head";
+import { Fragment } from "react";
 import ContactForm from "../components/contact/contact-form";
 
 function ContactPage() {
-  return <ContactForm />
+  return (
+  <Fragment>
+    <Head>
+      <title>Contact Me</title>
+      <meta 
+        name="description"
+        content="Send me your messages"
+      />
+    </Head>
+    <ContactForm />
+  </Fragment>
+  
+  )
 }
 
 export default ContactPage;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // 1. Hero section => Present ourselves
 // 2. Featured posts section
-
+import Head from "next/head";
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
@@ -10,6 +10,13 @@ import { getFeaturedPosts } from "../helpers/posts-util";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Mike's Blog</title>
+        <meta 
+          name="description"
+          content="I post about programming"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
