5b590ef7e03059d2fe30f4c1de1f79f6e310cb79
Mike Le <mike.le@live.com>
2022-12-01 16:18:57 -0800

    contact api working with mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,8 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
+    console.log("Handing API api/contact")
     const { email, name, message } = req.body;
 
     if (
@@ -23,11 +26,33 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://test:123@udemy-next.m9tdv89.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+
+    const db = client.db("my-site");
+    
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({message: 'Storing message failed'});
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({
       message: "Successfully stored message!",
-      message: newMessage
+      message: newMessage,
     });
   }
 }
